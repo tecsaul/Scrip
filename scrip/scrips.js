ba0142439f828b7bfd74ea392e7193fad6b8da84
saul nuñez <77266273+tecsaul@users.noreply.github.com>
2022-09-27 19:53:03 -0700

    Add files via upload

---
+++ scrip/scrips.js
@@ -0,0 +1,46 @@
+console.log("this is the console");
+
+
+
+let studentName = "frank"
+let Name = "Saul"
+let isActive = true
+let movie = "avatar"
+let age = 18
+let nocaminar = false 
+let categoria = "horror"
+let BuyTicket = true
+let DontBuy = false
+let Buyfood = true
+let apellido = "parra"
+let appelidoSeg = "nu;ez"
+let escuela = "ITT"
+let respira = true
+let comidapr = "burger"
+let colorfav = "azul"
+let zapatos = "cafes"
+let alumno = true 
+let PC = true 
+let dogs = "dogs"
+let cats = "cats"
+
+document.write("<p> name "+ studentName+ "</p>");
+document.write("<p> nombre del estudiante "+ Name+ "</p>");
+document.write("<p> confirmar "+ isActive+ "</p>");
+document.write("<p> pelicula "+ movie+ "</p>");
+document.write("<p> edad "+ age+ "</p>");
+document.write("<p> no puede caminar "+ false+ "</p>");
+document.write("<p> categoria de pelicula "+ categoria+ "</p>");
+document.write("<p> compraron los ticket "+ BuyTicket + "</p>");
+document.write("<p> no compro los boletos "+ DontBuy+ "</p>");
+document.write("<p> compraron comida "+ Buyfood+ "</p>");
+document.write("<p> Apellido "+ apellido+ "</p>");
+document.write("<p> Apellido secundario "+ appelidoSeg + "</p>");
+document.write("<p> escuela en la que estudia? "+ escuela+ "</p>");
+document.write("<p> siempre respira "+ respira+ "</p>");
+document.write("<p> comida preferida "+ comidapr+ "</p>");
+document.write("<p> color favorito "+ colorfav+ "</p>");
+document.write("<p> color de zapatos? "+ zapatos+ "</p>");
+document.write("<p> me gustan los "+ cats+ "</p>");
+document.write("<p> me gustan los  "+ dogs+ "</p>");
+
